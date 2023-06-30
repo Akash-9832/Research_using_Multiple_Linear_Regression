5d8dac001782bd7081e91806d89b238e996f3107
Akash Modak <118994261+Akash-9832@users.noreply.github.com>
2023-06-30 12:39:34 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 df=pd.read_excel(r'C:\Users\Akash\Desktop\nepal_data.xlsx')
 
 #***************************Training Segment*****************************
@@ -29,7 +28,7 @@ for i in range(3):
         q1.append(s2)
         t1=l2[i]*l1[i]
         p1.append(t1)
-    b0=sum(p1)/sum(q1)
+    b1=sum(p1)/sum(q1)
 
 
     c = df['total_DAG'].tolist()
@@ -45,7 +44,7 @@ for i in range(3):
         q2.append(s3)
         t2=l3[i]*l1[i]
         p2.append(t2)
-    b1=sum(p2)/sum(q2)
+    b2=sum(p2)/sum(q2)
 
 
     d = df['total_household'].tolist()
@@ -61,7 +60,7 @@ for i in range(3):
         q3.append(s4)
         t3=l4[i]*l1[i]
         p3.append(t3)
-    b2=sum(p3)/sum(q3)
+    b3=sum(p3)/sum(q3)
 
 
     f = df['www'].tolist()
@@ -77,7 +76,7 @@ for i in range(3):
         q5.append(s6)
         t5=l6[i]*l1[i]
         p5.append(t5)
-    b3=(sum(p5)/sum(q5))
+    b4=(sum(p5)/sum(q5))
 
 
     g = df['DRH'].tolist()
@@ -93,7 +92,7 @@ for i in range(3):
         q6.append(s7)
         t6=l7[i]*l1[i]
         p6.append(t6)
-    b4=(sum(p6)/sum(q6))
+    b5=(sum(p6)/sum(q6))
 
 
     h = df['DDH'].tolist()
@@ -109,7 +108,7 @@ for i in range(3):
         q7.append(s8)
         t7=l8[i]*l1[i]
         p7.append(t7)
-    b5=sum(p7)/sum(q7)
+    b6=sum(p7)/sum(q7)
 
 
     m = df['dist_gained'].tolist()
@@ -125,7 +124,7 @@ for i in range(3):
         q8.append(s9)
         t8=l9[i]*l1[i]
         p8.append(t8)
-    b6=sum(p8)/sum(q8)
+    b7=sum(p8)/sum(q8)
 
 
     n = df['river_type'].tolist()
@@ -135,35 +134,35 @@ for i in range(3):
     p9=[]
     q9=[]
     for i in range(xy_limit):
-        r9=(x9[i]-x9_bar)
-        s9=(x9[i]-x9_bar)**2
-        l10.append(r9)
-        q9.append(s9)
+        r10=(x9[i]-x9_bar)
+        s10=(x9[i]-x9_bar)**2
+        l10.append(r10)
+        q9.append(s10)
         t9=l10[i]*l1[i]
         p9.append(t9)
-    b7=sum(p9)/sum(q9)
+    b8=sum(p9)/sum(q9)
     
 #*********************************Testing Segment*************************************
-    
     count=tp=tn=fn=fp=0
     pred=[]
     ori=[]
-    for k in range(xy_limit+1,5801):
-        Y=(b0 + c[k]*b1 + d[k]*b2 + f[k]*b3 + g[k]*b4 + h[k]*b5 + m[k]*b6 + n[k]*b7)
-        if(abs(Y-a[k])<0.4):
+    b0=y_bar-(b1*x1_bar + b2*x2_bar + b3*x3_bar + b4*x5_bar + b5*x6_bar + b6*x7_bar + b7*x8_bar + b8*x9_bar)
+    for i in range(xy_limit+1,5801):
+        Y=(b0 + b[i]*b1 + c[i]*b2 + d[i]*b3 + f[i]*b4 + g[i]*b5 + h[i]*b6 + m[i]*b7 + n[i]*b8)
+        if(abs(Y-a[i])<0.4):
             count+=1
-        if(abs(Y)<=0.4):
+        if(abs(Y)<0.3):
             pred.append(0)
-        if(Y>0.4):
+        if(Y>0.3):
             pred.append(1) 
     acc=round(((count*100)/(5800-xy_limit)),3)
     ios.append(acc)
     for i in range(xy_limit+1,5801):
-        if(a[i]<=0.4):
-            a[i]=0
-        elif(a[i]>0.4):
-            a[i]=1
-        ori.append(a[i])
+        if(a[i]>0):
+            ori.append(1)
+        elif(a[i]<0.3):
+            #a[i]=1
+           ori.append(0)
     for j in range(len(pred)):
         if(ori[j]==1 and pred[j]==1):
             tp+=1
@@ -173,59 +172,29 @@ for i in range(3):
             fp+=1
         elif(ori[j]==0 and pred[j]==1):
             fn+=1
+        
     print('TP=',tp,'  ','TN=',tn)
     print('FP=',fp,'  ','FN=',fn)
     EA=((tp+fp)*(tp+fn)+(fn+tn)*(fp+tn))/100
-    
+
     sen=tp/(tp+fn)
-    print('Sensitivity=',sen)
+    print('Sensitivity=',round(sen,2))
 
     spe=tn/(tn+fp)
-    print('Specificity=',spe)
+    print('Specificity=',round(spe,2))
 
     prec=tp/(tp+fp)
-    print('Precision=',prec)
+    print('Precision=',round(prec,2))
 
     recall=tp/(tp+fn)
-    print('Recall=',recall)
+    print('Recall=',round(recall,2))
+
+    acc2=((tp+tn)/(tp+tn+fp+fn))*100
+    print('Accuracy(CM)= ',round(acc2,2))
 
     f1_score=(2*recall*prec)/(recall+prec)
-    print('F1 Score=',f1_score)
-    
-    acc2=((tp+tn)/(tp+tn+fp+fn))*100
-    print('Accuracy(CM)= ',round(acc2,3))
-    
+    print('F1 Score=',round(f1_score,2))
+
     kap=(acc2-EA)/(100-EA)
-    print('Kappa Test=',kap)
-    plt.plot(tp,fp)
-    plt.ylabel('True positive rate')
-    plt.xlabel('False positive rate')
-    plt.show()
+    print('Kappa Test=',round(kap,1))
     print("\n")
-for i in range(3):
-    print("Accuracy as",audi[i],"% training data= ",ios[i])
-
-
-
-
-
-
-
-   
-
-
-'''
-
-
-        
-
-
-a = df['technical_feas'].tolist()
-for i in range(5807):
-    if(a[i]=='Feasible'):
-        a[i]=1
-    else:
-        a[i]=0
-        #print(Y, a[k])
-        #P=1/(1+(2.718**Y))
-        #print(P)'''
